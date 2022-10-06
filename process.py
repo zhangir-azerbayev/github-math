0aa274f61f235f639035720f658f19bc76538028
Zhangir Azerbayev <zazerbayev@gmail.com>
2022-10-06 02:21:13 -0400

    python stuff not scaling

--- process.py
+++ process.py
@@ -13,6 +13,55 @@ RAW_DIR = "raw_files"
 PROCESSED_DIR = "processed"
 MIN_STARS = 5
 
+def create_stars_of_repo(tuples, creds): 
+    """
+    `tuples` is a list of (owner, repo_name)
+
+    the key for a repo is "repo" + {hash of owner/repo_name}
+    """
+
+    subqueries = [
+            f"""\
+    repo{i}: repository(owner: "{x[0]}", name: "{x[1]}") {{
+        ...repoProperties
+    }}""" for i, x in enumerate(tuples)
+            ]
+
+    key_of_repo = {x[0] + "/" + x[1]: "repo" + str(i) for i, x in enumerate(tuples)}
+
+    query = """\
+fragment repoProperties on Repository {
+    stargazers {
+            totalCount
+        }
+    }
+
+query {
+""" + "\n".join(subqueries) + "\n}"
+
+    resp = requests.post(
+            url = "https://api.github.com/graphql", 
+            json = {"query": query}, 
+            auth=creds
+            )
+
+    print(resp)
+
+    resp_json = json.loads(resp.content.decode('utf-8'))["data"]
+    
+    print(resp_json)
+
+    stars_of_key = {x: resp_json[x]['stargazers']['totalCount'] for x in resp_json if resp_json[x]}
+
+    stars_of_repo = {repo: stars_of_key[key_of_repo[repo]] for repo in key_of_repo if key_of_repo[repo] in stars_of_key}
+    
+    print("NO METADATA FOR FOLLOWING REPOS FOUND")
+    no_metadata = [repo for repo in key_of_repo if key_of_repo[repo] not in stars_of_key]
+    print(no_metadata)
+
+    return stars_of_repo
+
+        
 def process_files(archives_dir, 
                   dest_dir, 
                   substrings,
@@ -25,16 +74,20 @@ def process_files(archives_dir,
     Path(dest_dir).mkdir(parents=True, exist_ok=True)
 
     new_data = []
-    stars_of_repo = dict()
-    not_founds = set()
 
     count_raw = 0
     count_proc = 0
+    new_count_proc = 0 
 
     for archive in tqdm(os.listdir(archives_dir)):
         with gzip.open(os.path.join(archives_dir, archive)) as f: 
             data = ndjson.load(f)
+    
+        # builds hashmap from repo_name to number of stars
+        tuples = [y.split("/") for y in list(set([x["repo_name"] for x in data]))]
+        stars_of_repo = create_stars_of_repo(tuples, creds)
 
+        # filters out source files that don't contain keywords
         if substrings:
             data = list(filter(lambda x: any(y in x["content"] for y in substrings), data))
  
@@ -43,26 +96,8 @@ def process_files(archives_dir,
 
             if repo_name in stars_of_repo: 
                 stars = stars_of_repo[repo_name]
-            elif repo_name not in not_founds: 
-                resp = requests.get("https://api.github.com/repos/" + repo_name, 
-                        auth=creds)
-                if resp.status_code == 404: 
-                    not_founds.add(repo_name)
-                    continue
-                elif resp.status_code == 451: 
-                    # Unavailable for legal reasons
-                    not_founds.add(repo_name)
-                    continue
-                elif resp.status_code != 200: 
-                    print("WARNING: HTTP REQUEST FAILED")
-                    print(f"RESP STATUS CODE{resp.status_code}")
-                    print(repo_name)
-                    not_founds.add(repo_name)
-                    continue 
-
-                metadata = json.loads(resp.content.decode("utf-8"))
-                stars = metadata["stargazers_count"]
-                stars_of_repo[repo_name] = stars
+            else: 
+                continue 
 
             text = line["content"]
             
@@ -71,6 +106,7 @@ def process_files(archives_dir,
                 meta["stars"] = stars
                 new_line = {"text": text, "meta": meta}
                 new_data.append(new_line)
+            
 
         save_path = os.path.join(dest_dir, archive.replace(".gz", ".zst"))
         json_str = ndjson.dumps(new_data)
@@ -84,10 +120,9 @@ def process_files(archives_dir,
         count_proc += len(new_data)
 
     
-    print(f"FOLLOWING REPOS 404'ED: {not_founds}")
     print(f"RAW FILES ANALYZED: {count_raw}")
     print(f"PROCESSED FILES PRODUCED: {count_proc}")
-    
+ 
     return
 
 def process_lean(creds): 
